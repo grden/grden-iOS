11556e08a7e6fa30cfa9d7525e90195ba0d0646b
Jeongwon Kim <mickykim4@gmail.com>
2024-04-11 11:56:09 +0900

    initial commit

--- TestApp/ViewController.swift
+++ TestApp/ViewController.swift
@@ -7,11 +7,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+extension UISearchBar {
+    func setTextFieldColor(_ color: UIColor) {
+        for subView in self.subviews {
+            for subSubView in subView.subviews {
+                let view = subSubView as? UITextInputTraits
+                if view != nil {
+                    let textField = view as? UITextField
+                    textField?.backgroundColor = color
+                    break
+                }
+            }
+        }
+    }
+}
 
+class ViewController: UIViewController {
+    @IBOutlet weak var TabBar: UITabBar!
+    @IBOutlet weak var SearchBar: UISearchBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        TabBar.layer.cornerRadius = 16
+        
+        SearchBar.placeholder = "찾는 맛집 이름이 뭐예요?"
+//        SearchBar.setTextFieldColor(UIColor.white)
+//        SearchBar.searchTextField.layer.cornerRadius = 0
     }
 
 
